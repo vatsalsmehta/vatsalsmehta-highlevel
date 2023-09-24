3b84160590718be45611329ff9bcad5820a7dad6
vatsal-s-mehta <vatsal.sameer@udaan.com>
2023-09-24 18:13:38 +0530

    UI Improvements

--- frontend-v1/pages/newLogin/index.tsx
+++ frontend-v1/pages/newLogin/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { useState } from 'react';
-import { useRouter } from 'next/router'; 
-import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
+import { useRouter } from 'next/router';
+import { Box, Button, FormControl, InputLabel, MenuItem, Select, TextField, Typography } from '@mui/material';
 import { prodUrl } from '../../models/transactionModels';
 
 const NewLogin = () => {
-  const router = useRouter(); // Initialize the router
+  const router = useRouter();
   const [name, setName] = useState('');
   const [balance, setBalance] = useState(0);
   const [email, setEmail] = useState('');
@@ -23,13 +23,11 @@ const NewLogin = () => {
   };
 
   const handleCreateWallet = async () => {
-    // Check if all inputs are present
     if (name.trim() === '' || balance <= 0 || email.trim() === '') {
       alert('Please fill in all fields.');
       return;
     }
 
-    // Make an API call
     const apiUrl = `${prodUrl}/initialise/wallet`;
     const requestBody = {
       name: name,
@@ -50,52 +48,57 @@ const NewLogin = () => {
         throw new Error('Server Error: Failed to create wallet');
       }
 
-      // Handle success, e.g., redirect to a dashboard page
       alert('Wallet created successfully.');
       localStorage.setItem('email_id', email);
 
-      router.push('/dashboard'); 
-      // Redirect or navigate to another page here if needed
+      router.push('/dashboard');
     } catch (error: any) {
       alert('Error creating wallet: ' + error.message);
     }
   };
 
   return (
-    <div>
-      <Box sx={{ minWidth: 200 }}>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <Box sx={{ minWidth: 200, textAlign: 'center', padding: '20px' }}>
+        <Typography variant="h5" style={{ marginBottom: '20px', color: 'white' }}>
+          Create a New Wallet
+        </Typography>
+        <Typography variant="body1" style={{ marginBottom: '20px', color: 'white' }}>
+          Enter your Name, initial Balance Amount, and Email
+        </Typography>
         <TextField
           id="name"
-          label="Name"
+          placeholder="Name"
           variant="outlined"
           fullWidth
           value={name}
           onChange={handleNameChange}
-          style={{ marginTop: '20px' }}
+          style={{ marginBottom: '20px' }}
         />
         <TextField
           id="balance"
-          label="Balance"
+          placeholder="Balance"
           type="number"
           variant="outlined"
           fullWidth
           value={balance}
           onChange={handleBalanceChange}
-          style={{ marginTop: '20px' }}
+          style={{ marginBottom: '20px' }}
         />
         <TextField
           id="email"
-          label="Email"
           variant="outlined"
           fullWidth
+          placeholder='Email'
           value={email}
           onChange={handleEmailChange}
-          style={{ marginTop: '20px' }}
+          style={{ marginBottom: '20px' }}
         />
         <Button
-          style={{ marginTop: '20px', marginLeft: '35px', marginBottom: '30px' }}
           variant="contained"
+          color="primary"
           onClick={handleCreateWallet}
+          style={{ marginTop: '20px', minWidth: '150px' }}
         >
           Create Wallet
         </Button>
